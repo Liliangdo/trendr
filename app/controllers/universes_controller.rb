0a0409c34a0894d2c56047361abd0b9570df78c0
lilian garrido <lilian.garrido13@gmail.com>
2016-10-11 15:42:55 +0200

    Add the strong params for the controller Universes, plus create the Universes CRUD

--- app/controllers/universes_controller.rb
+++ app/controllers/universes_controller.rb
@@ -1,2 +1,35 @@
 class UniversesController < ApplicationController
+
+before_action :find_universe, only: [:show, :new, :create, :destroy]
+
+  def index
+    Universe.all
+  end
+
+  def show
+
+  end
+  def new
+    @universe = Universe.new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def find_universe
+    @universe = Universe.find(params[:id])
+  end
+
+  def universe_params
+    params.require(:universe).permit(
+      :name,
+      :price,
+      :photo)
+  end
 end
