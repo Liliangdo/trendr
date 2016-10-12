43aa199fb948840bdaaca079f43f135c12a5e08a
lilian garrido <lilian.garrido13@gmail.com>
2016-10-12 10:40:18 +0200

    add the card listed in the home page with the front

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def helper_picture_url(universe)
+    if universe.photo?
+      cl_image_path(universe.photo.path)
+    else
+      "http://static.wixstatic.com/media/b6a999_c97c7e56d74149a9aed270cc83927158.jpg"
+    end
+  end
 end
