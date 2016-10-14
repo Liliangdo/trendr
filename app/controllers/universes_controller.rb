49c064fb933114ca7c9b84812211f21d4d0f337a
lilian garrido <lilian.garrido13@gmail.com>
2016-10-14 14:26:07 +0200

    add the Universe pickers on the home page

--- app/controllers/universes_controller.rb
+++ app/controllers/universes_controller.rb
@@ -1,5 +1,5 @@
 class UniversesController < ApplicationController
-
+skip_before_action :authenticate_user!, only: [:index, :show]
 before_action :find_universe, only: [:show, :destroy]
 
   def index
